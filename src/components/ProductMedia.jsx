6de117dfe5287a58c60dfbcf082d8374c6a7a979
Jorge <jorge.aldeleo@gmail.com>
2026-08-19 16:51:42 -0300

    fotos mejor

--- src/components/ProductMedia.jsx
+++ src/components/ProductMedia.jsx
@@ -1,4 +1,4 @@
 export default function ProductMedia({ product }) {
-  if (product.img) return <img className="prod-photo" src={product.img} alt={product.name} />;
+  if (product.img) return <img className="prod-photo" src={product.img} alt={product.name} draggable="false" />;
   return product.icon;
 }
